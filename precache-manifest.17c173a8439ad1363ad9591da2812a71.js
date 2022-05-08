c1cbd2c86e5718a98b7a4f62b6fb764a6f0c23fa
Andrii <67384378+Andrii-Toporivskyi@users.noreply.github.com>
2022-05-08 15:12:21 +0300

    Add files via upload

---
+++ precache-manifest.17c173a8439ad1363ad9591da2812a71.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2e9f3c13b3f833034a12",
+    "url": "/static/css/main.89986fbc.chunk.css"
+  },
+  {
+    "revision": "2e9f3c13b3f833034a12",
+    "url": "/static/js/main.2e9f3c13.chunk.js"
+  },
+  {
+    "revision": "f643efff6e433eee2960",
+    "url": "/static/js/1.f643efff.chunk.js"
+  },
+  {
+    "revision": "643f4e098202074b50a0",
+    "url": "/static/js/2.643f4e09.chunk.js"
+  },
+  {
+    "revision": "59d4509d10f4f0b182ff",
+    "url": "/static/js/runtime~main.59d4509d.js"
+  },
+  {
+    "revision": "b3bb9e7174890174af5f75bdd47e8c0c",
+    "url": "/static/media/fon.b3bb9e71.jpg"
+  },
+  {
+    "revision": "882173c4c68cfb0a893108689e9e04d9",
+    "url": "/index.html"
+  }
+];
